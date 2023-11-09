61abb4b1021c5b8d66350da60d6d6679a6974c0c
GPU <gpungeot+dev@gmail.com>
2023-11-09 12:40:48 +0100

    first view

--- src/PEAWeb/urls.py
+++ src/PEAWeb/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from stock.views import stocks
 
 urlpatterns = [
+    path('', stocks),
     path('admin/', admin.site.urls),
 ]
